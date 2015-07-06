e16bf7099bbd2325663f48b06bb8ab9c8e7dbfd6
GaoMjun <jun2675731483@gmail.com>
2015-07-06 14:26:26 +0800

    add day5_pwm

---
+++ day5_pwm/MDKProj/output/system_stm32f10x.d
@@ -0,0 +1,17 @@
+.\output\system_stm32f10x.o: ..\cmsis\system_stm32f10x.c
+.\output\system_stm32f10x.o: ..\cmsis\stm32f10x.h
+.\output\system_stm32f10x.o: ..\cmsis\core_cm3.h
+.\output\system_stm32f10x.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\output\system_stm32f10x.o: ..\cmsis\system_stm32f10x.h
+.\output\system_stm32f10x.o: ..\user\stm32f10x_conf.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_crc.h
+.\output\system_stm32f10x.o: ..\cmsis\stm32f10x.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_dma.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_exti.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_gpio.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_i2c.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_rcc.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_rtc.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_tim.h
+.\output\system_stm32f10x.o: ..\lib\inc\stm32f10x_usart.h
+.\output\system_stm32f10x.o: ..\lib\inc\misc.h
---
+++ day5_pwm/user/main.c
@@ -0,0 +1,84 @@
+#include "stm32f10x.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "bsp.h"
+#include "led.h"
+#include "delay.h"
+#include "uart1.h"
+#include "encoder.h"
+#include "motor.h"
+
+#include "consola.h"
+
+char Uart1_Buffer[0xFF] = "0";
+char command1[0xFF] = "led on";
+char command2[0xFF] = "led off";
+uint16_t Uart1_BufferSize = 0;
+uint8_t i = 0;
+uint8_t commandEnd = 0;
+
+char test[0xFF];
+int main(void)
+{
+	SystemInit();
+	bsp_init();
+	led_init();
+	delay_init();
+	uart1_init();
+	encoder_init();
+	motor_init();
+	
+	NVIC_uart1_Configuration();
+	NVIC_encoder_Configuration();
+	
+	while(1)
+	{
+		led_on();
+		DelayMs(500);
+		led_off();
+		DelayMs(500);
+		
+		set_motor_speed((uint16_t)449, (uint16_t)0);
+		
+		if(commandEnd)
+		{
+			commandEnd = 0;
+			
+			Uart1_Buffer[Uart1_BufferSize-1] = '\0';
+			if(0 == strcmp(Uart1_Buffer, command1))
+				led_on();
+			else if(!strcmp(Uart1_Buffer, command2))
+				led_off();
+			else
+				printf("unknow command \r\n");
+			
+			Uart1_BufferSize = 0;
+		}		
+		printf("left:%d    right:%d \r\n", get_left_encoder_num(), get_right_encoder_num());
+	}
+}
+	
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+
+#endif
+
